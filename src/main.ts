e50a16dccdeca6aabf52a405ae97016895a03893
panji <panji@aviana.co.id>
2025-03-15 10:51:16 +0800

    up dev

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+   // logger: WinstonModule.createLogger(winlogger),
+    cors: true,
+  });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const configservice = app.get(ConfigService);
+
+  await app.listen(configservice.get('PORT') || 3000);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log(`App started PORT ${process.env.PORT || 3000}`);
+});
